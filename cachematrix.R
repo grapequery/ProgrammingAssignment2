345329fe897b02204680bb573a14b3d725d9187a
grapequery <grapequery@gmail.com>
2015-06-21 16:39:46 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following code contains two functions
+## User must enter variable name for the matrix
+## The first function creates a matrix and caches its inverse
+## The second function returns the inverse of a matrix
+## Both functions assume the matrix is invertible
 
-## Write a short comment describing this function
+## Definition: matrix(data = x, nrow = nrow, ncol = ncol, default is byrow = FALSE,dimnames = NULL)
+# Sample 4 x 4 and 1 x 1 numeric matrix for testing function
+# X1 <- matrix(c(0,1,2,0), nrow = 2, ncol = 2, byrow = FALSE)
+# X2 <- matrix(c(6,5,1,2), nrow = 2, ncol = 2, byrow = FALSE)
+# X=2
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##This function creates a matrix object and caches its inverse.
+makeCacheMatrix <- function(x=matrix()){ 
+    a<-x
+    y<-solve(x)
+    save(a,y,file="all.RData") 
+    rm(a)
+    rm(y)  
 }
 
+## This function returns a matrix that is the inverse of 'x'.
+## The cached inverse is returned if it has already been calculated for x 
+##   or if inverse is not cached, it is calculated and returned
+## This function also saves the calculated inverse in data file to further minimize
+##   subsequent computer resource needs 
+cacheSolve <- function(x=matrix(),...) { 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    load("all.RData") 
+    if(a==x){
+        message("returning cached inverse")
+        return(y)
+    }
+    if(a!=x){
+        a<-x
+        y<-solve(x)
+        message("returning calculated inverse")
+        save(a,y,file="all.RData")
+        rm(a)
+        return(y)
+    }         
 }
